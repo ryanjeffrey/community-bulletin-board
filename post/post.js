6e95e21e2926f71f4544e19af0da82b88620c6fd
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-17 19:58:11 -0500

    Create conditional statement to match user id

--- post/post.js
+++ post/post.js
@@ -1,7 +1,10 @@
-import { getPost } from '../fetch-utils.js';
+import { checkAuth, getPost } from '../fetch-utils.js';
 import { renderPostDetail } from '../render-utils.js';
 
 const postDetailSection = document.getElementById('post-detail-section');
+
+const user = checkAuth();
+console.log(user.id);
 
 const params = new URLSearchParams(window.location.search);
 
@@ -14,6 +17,14 @@ async function loadData() {
     // render and append this post details to the container
     const postDetail = renderPostDetail(post);
     postDetailSection.append(postDetail);
+
+    console.log(post.user_id);
+
+    if (user.id === post.user_id) {
+        console.log('deletable');
+    } else {
+        console.log(`cannot delete another user's post`);
+    }
 }
 
 loadData();
